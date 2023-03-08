08508c544f0e3c72c5a2e65cf0105b33211bcedf
Andrius Paulauskas <paulauskas.andrius1@gmail.com>
2023-03-09 01:13:21 +0200

    Add files via upload

---
+++ Reiz/Branch.cs
@@ -0,0 +1,36 @@
+﻿public class Branch
+{
+    private List<Branch> branches;
+
+    private Branch()
+    {
+        this.branches = new List<Branch>();
+    }
+
+    public static void Main(string[] args)
+    {
+        Branch main = new Branch();
+        main.branches.Add(new Branch());
+        main.branches.Add(new Branch());
+        main.branches[0].branches.Add(new Branch());
+        main.branches[1].branches.Add(new Branch());
+        main.branches[1].branches.Add(new Branch());
+        main.branches[1].branches.Add(new Branch());
+        main.branches[1].branches[0].branches.Add(new Branch());
+        main.branches[1].branches[1].branches.Add(new Branch());
+        main.branches[1].branches[1].branches.Add(new Branch());
+        main.branches[1].branches[1].branches[0].branches.Add(new Branch());
+
+        Console.WriteLine("Structure depth: " + Convert.ToString(GetDepth(main, 1, 0)));
+    }
+
+    private static int GetDepth(Branch main, int currDepth, int maxDepth)
+    {
+        foreach (var branch in main.branches)
+        { 
+            maxDepth = GetDepth(branch, currDepth + 1, maxDepth);
+        }
+
+        return (currDepth > maxDepth) ? currDepth : maxDepth;
+    }
+}
---
+++ Reiz/Clock.cs
@@ -0,0 +1,42 @@
+﻿public class Clock
+{
+    public static void Main(string[] args)
+    {
+        short hour, minute;
+
+        Console.WriteLine("This is an analogue clock.\nPossible hour values are 1 - 12 and minute values are 0-59");
+        
+        while (true)
+        {
+            Console.WriteLine("Input the hour value: ");
+            hour = Convert.ToInt16(Console.ReadLine());
+            if (hour is > 0 and <= 12)
+            {
+                break;
+            }
+            Console.WriteLine("Please use numbers in the range of: 1 - 12");
+        }
+
+        while (true)
+        {
+            Console.WriteLine("Input the minute value: ");
+            minute = Convert.ToInt16(Console.ReadLine());
+            if (minute is >= 0 and < 60)
+            {
+                break;
+            }
+            Console.WriteLine("Please use numbers in the range of: 0 - 59");
+        }
+
+        Console.WriteLine("The angle between the arrows is " + Convert.ToString(GetAngle(hour, minute)) + " degrees.");
+    }
+
+
+    private static double GetAngle(short hour, short minute)
+    {
+        var arrowHour = (Convert.ToDouble(hour) * 30) + (Convert.ToDouble(minute) / 60 * 30);
+        var arrowMinute = Convert.ToDouble(minute) * 360 / 60;
+        var difference = Math.Abs(arrowHour - arrowMinute);
+        return (difference < 180) ? (difference) : (360 - difference);
+    }
+}
